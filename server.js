b7503e87f22d1fc8e04f71de7f8dd61f548ec1d2
PJA0103 <anne0103@outlook.com>
2025-02-03 21:41:58 +0800

    add UUID add method OPTIONS and POST

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const http = require("http");
-
+const { v4: uuidv4 } = require("uuid");
+const todos = [];
 const requestListener = (request, response) => {
-    const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
-        "Content-type": "application/json"
-    }
-    console.log(request.url);
-    if (request.url =="/" && request.method =="GET"){
-        response.writeHead(200,headers); 
+const headers = {
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+    "Content-type": "application/json"
+}
+
+    if (request.url =="/todos" && request.method =="GET"){
+        response.writeHead(200, headers); 
         response.write(JSON.stringify({
             "status": "success",
-            "data": [],
+            "data": todos,
         }));
         response.end();
+    }else if (request.url =="/todos" && request.method =="POST"){
+        response.writeHead(200, headers);
+        response.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+        }));
+        response.end();
+    }else if (request.method =="OPTIONS"){
+        response.writeHead(200, headers);
+        response.end();
     }else{
-        response.writeHead(404,headers);
+        response.writeHead(404, headers);
         response.write(JSON.stringify({
             "status": "false",
             "message": "沒有這個路徑",
