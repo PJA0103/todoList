384f1e2cb00346fa404a0e6384f5f062905c1de2
PJA0103 <anne0103@outlook.com>
2025-02-04 18:04:13 +0800

    request on

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const http = require("http");
 const { v4: uuidv4 } = require("uuid");
 const todos = [];
 const requestListener = (request, response) => {
-const headers = {
-    "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
-    "Content-type": "application/json"
-}
-
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+        "Content-type": "application/json"
+    }
+    let body ="";
+    request.on("data", chunk =>{
+        body+= chunk;
+    })
+    request.on("end", () =>{
+        console.log(JSON.parse(body).title);
+    })
     if (request.url =="/todos" && request.method =="GET"){
         response.writeHead(200, headers); 
         response.write(JSON.stringify({
